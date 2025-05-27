1c7c246ade576bded5e4b7ced2f0c9329b35ec06
JDoddsNAIT <144161210+JDoddsNAIT@users.noreply.github.com>
2025-05-27 12:01:37 -0600

    Added transient values to Player motion manager

--- Slow Jam 3/Assets/Scripts/Player/Movement/PlayerMotionManger.cs
+++ Slow Jam 3/Assets/Scripts/Player/Movement/PlayerMotionManger.cs
@@ -1,6 +1,7 @@
 using System;
 using UnityEngine;
 using Utilities.Extensions;
+using Utilities.Serializables;
 
 [RequireComponent(typeof(Rigidbody2D))]
 public class PlayerMotionManger : MonoBehaviour
@@ -13,6 +14,8 @@ public class PlayerMotionManger : MonoBehaviour
 	[Space]
 	[Tooltip("The controllers managed by this script. Each controller is processed in order of the array.")]
 	public PlayerMotionController[] controllers;
+	[Tooltip("These values persist for a single frame, where all controllers may access them at any time.")]
+	[SerializeField] private Dictionary<string, float> _transients = new();
 
 	public void Start()
 	{
@@ -54,4 +57,81 @@ public class PlayerMotionManger : MonoBehaviour
 	{
 		controllers = this.Get().Components<PlayerMotionController>().InChildren();
 	}
+
+	#region Transient Accessors
+	public void SetTransient(string name, bool value) => SetTransient(name, value ? 1 : 0);
+
+	public void SetTransient(string name, bool? value) => SetTransient(name, value switch { true => 1, null => 0, false => -1 });
+
+	public void SetTransient(string name, int value) => SetTransient(name, (float)value);
+
+	public void SetTransient(string name, float value)
+	{
+		if (_transients.ContainsKey(name))
+		{
+			_transients[name] = value;
+		}
+		else
+		{
+			_transients.Add(name, value);
+		}
+	}
+
+	public void GetTransient(string name, out bool value)
+	{
+		if (!_transients.ContainsKey(name))
+		{
+			value = default;
+			return;
+		}
+
+		GetTransient(name, out float result);
+		value = result switch {
+			> 0 => true,
+			< 0 => false,
+			_ => false
+		};
+	}
+
+	public void GetTransient(string name, out bool? value)
+	{
+		if (!_transients.ContainsKey(name))
+		{
+			value = default;
+			return;
+		}
+
+		GetTransient(name, out float result);
+		value = result switch {
+			> 0 => true,
+			0 => null,
+			< 0 => false,
+			float.NaN => throw new NotFiniteNumberException(result)
+		};
+	}
+
+	public void GetTransient(string name, out int value)
+	{
+		if (!_transients.ContainsKey(name))
+		{
+			value = default;
+			return;
+		}
+
+		GetTransient(name, out float result);
+		value = (int)result;
+	}
+
+	public void GetTransient(string name, out float value)
+	{
+		if (_transients.ContainsKey(name))
+		{
+			value = _transients[name];
+		}
+		else
+		{
+			value = default;
+		}
+	}
+	#endregion
 }
