a20c4db24dfd97a97fddd282c7e36160388f2a29
JDoddsNAIT <144161210+JDoddsNAIT@users.noreply.github.com>
2025-05-28 13:19:56 -0600

    First test of jump trajectory drawer

--- Slow Jam 3/Assets/Scripts/Player/JumpTrajectoryDrawer.cs
+++ Slow Jam 3/Assets/Scripts/Player/JumpTrajectoryDrawer.cs
@@ -3,46 +3,84 @@ using System.Collections.Generic;
 using Systems.Stats;
 using UnityEngine;
 
+[ExecuteInEditMode]
 public class JumpTrajectoryDrawer : MonoBehaviour
 {
 	public PlayerMoveStats stats;
 	public Vector2 origin;
 
-	private List<Vector2> _points = new();
-
 	public const float HUNDREDTHS_TO_SECONDS = 0.01f;
 
+	private StatFloat _gravity;
 	private StatFloat _xVelocity, _yVelocity;
-	private StatFloat _maxXVelocity, _maxYVelocity;
+	private StatMultiplier _reverseDirection;
+	private StatMultiplier _fallingGravity;
+	private StatClamp _maxFallSpeed;
 
-	private StatClamp _xClamp, _yClamp;
+	private RaycastHit2D _raycastHit;
+
+	private Vector2 Velocity => new(_xVelocity.ModifiedValue, _yVelocity.ModifiedValue);
+
+	public void Awake()
+	{
+		// default values are set elsewhere
+		_gravity = new StatFloat(baseValue: default, _fallingGravity);
+		_xVelocity = new StatFloat(baseValue: default, _reverseDirection);
+		_yVelocity = new StatFloat(baseValue: default, _maxFallSpeed);
+
+		_reverseDirection = new StatMultiplier(multiplier: -1, enabled: false);
+		_fallingGravity = new StatMultiplier(multiplier: default, enabled: true);
+		_maxFallSpeed = new StatClamp(range: default, enabled: true);
+	}
 
 	public void OnDrawGizmos()
 	{
 		if (stats == null || stats.jumpTrajectory.Hide)
 			return;
 		var settings = stats.jumpTrajectory;
+
+		Gizmos.color = settings.Color;
 		
-		_points.Clear();
-		_points.Capacity = settings.MaxLength;
-		
+		// Physics variables
 		var timeStep = (settings.MaxLength * HUNDREDTHS_TO_SECONDS) / settings.Resolution;
+		Vector2 position = (Vector2)transform.position + origin, displacement;
 
-		// V = (ground speed, jump speed)
-		
-		// D = (origin + transform position)
-		Vector2 position = (Vector2)transform.position + origin;
+		_xVelocity.BaseValue = stats.MaxSpeed.BaseValue;
+		_yVelocity.BaseValue = stats.JumpSpeed.BaseValue;
+		stats.CalculateGravity();
+		_gravity.BaseValue = stats.Gravity.BaseValue;
 
-		_points.Add(position);
+		// stat modifiers
+		_fallingGravity.Multiplier = stats.FallingGravity.Multiplier;
+		_maxFallSpeed.Min = -stats.MaxFallSpeed.BaseValue;
+		_maxFallSpeed.Max = +stats.MaxFallSpeed.BaseValue;
 
+		_reverseDirection.Enabled = stats.jumpTrajectory.direction is JumpTrajectoryGizmoSettings.Direction.Left;
+		_fallingGravity.Enabled = false;
 
-		/*
-		 * Each step:
-		 * apply gravity up to max fall speed. if descending, apply the falling modifier
-		 * apply drag up to max air speed
-		 * move the position by the velocity
-		 * check for collisions, and break out if any.
-		 * otherwise, repeat.
-		 */
+		for (int i = 1; i < settings.Resolution; i++)
+		{
+			_fallingGravity.Enabled = _yVelocity.ModifiedValue < 0;
+
+			// Apply gravity and calculate displacement
+			_yVelocity.BaseValue -= _gravity.ModifiedValue * timeStep / 2;
+			displacement = Velocity * timeStep;
+			_yVelocity.BaseValue -= _gravity.ModifiedValue * timeStep / 2;
+
+			_raycastHit = Physics2D.Raycast(position, displacement.normalized, displacement.magnitude, stats.GroundLayer);
+
+			if (_raycastHit.collider)
+			{
+				displacement = displacement.normalized * _raycastHit.distance;
+			}
+
+			Gizmos.DrawLine(position, position + displacement);
+			position += displacement;
+
+			if (!_raycastHit.collider)
+				continue;
+			else
+				break;
+		}
 	}
 }
