020596218042a8adbcfb7d804886af9b522ed5e1
Derrik <heyimderrik@gmail.com>
2025-06-02 05:56:26 -0400

    Update TriggerEvent.cs

--- Slow Jam 3/Assets/Scripts/level/TriggerEvent.cs
+++ Slow Jam 3/Assets/Scripts/level/TriggerEvent.cs
@@ -3,48 +3,51 @@ using UnityEngine.Events;
 
 public class TriggerEvent : MonoBehaviour
 {
-	[SerializeField] private string _targetTag = "Player";
-	[SerializeField] private UnityEvent<GameObject> _onEnter;
-	[SerializeField] private UnityEvent<GameObject> _onStay;
-	[SerializeField] private UnityEvent<GameObject> _onExit;
+    [SerializeField] private string _targetTag = "Player";
+    [SerializeField] private UnityEvent<GameObject> _onEnter;
+    [SerializeField] private UnityEvent<GameObject> _onStay;
+    [SerializeField] private UnityEvent<GameObject> _onExit;
 
-	public event UnityAction<GameObject> OnEnter {
-		add => _onEnter.AddListener(value);
-		remove => _onEnter.RemoveListener(value);
-	}
+    public event UnityAction<GameObject> OnEnter
+    {
+        add => _onEnter.AddListener(value);
+        remove => _onEnter.RemoveListener(value);
+    }
 
-	public event UnityAction<GameObject> OnStay {
-		add => _onStay.AddListener(value);
-		remove => _onStay.RemoveListener(value);
-	}
+    public event UnityAction<GameObject> OnStay
+    {
+        add => _onStay.AddListener(value);
+        remove => _onStay.RemoveListener(value);
+    }
 
-	public event UnityAction<GameObject> OnExit {
-		add => _onExit.AddListener(value);
-		remove => _onExit.RemoveListener(value);
-	}
+    public event UnityAction<GameObject> OnExit
+    {
+        add => _onExit.AddListener(value);
+        remove => _onExit.RemoveListener(value);
+    }
 
-	private void OnTriggerEnter2D(Collider2D other)
-	{
-		if (other.gameObject.CompareTag(_targetTag))
-		{
-			_onEnter.Invoke(other.gameObject);
-		}
-	}
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag(_targetTag))
+        {
+            _onEnter.Invoke(other.gameObject);
+        }
+    }
 
-	private void OnTriggerStay2D(Collider2D other)
-	{
-		if (other.gameObject.CompareTag(_targetTag))
-		{
-			_onEnter.Invoke(other.gameObject);
-		}
-	}
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag(_targetTag))
+        {
+            _onStay.Invoke(other.gameObject);
+        }
+    }
 
-	private void OnTriggerExit2D(Collider2D other)
-	{
-		if (other.gameObject.CompareTag(_targetTag))
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag(_targetTag))
 
-		{
-			_onExit.Invoke(other.gameObject);
-		}
-	}
+        {
+            _onExit.Invoke(other.gameObject);
+        }
+    }
 }
