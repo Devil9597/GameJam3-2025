2299e65aa199e379152d8a392f12484a41b1d29b
Derrik <heyimderrik@gmail.com>
2025-06-03 01:57:56 -0400

    bucket

--- Slow Jam 3/Assets/Scripts/level/CollisionEvent.cs
+++ Slow Jam 3/Assets/Scripts/level/CollisionEvent.cs
@@ -4,50 +4,71 @@ using UnityEngine.Events;
 
 namespace level
 {
-	public class CollisionEvent : MonoBehaviour
-	{
-		[SerializeField] protected string _targetTag = "Player";
-		[SerializeField] protected UnityEvent<GameObject> _onEnter;
-		[SerializeField] protected UnityEvent<GameObject> _onStay;
-		[SerializeField] protected UnityEvent<GameObject> _onExit;
+    public class CollisionEvent : MonoBehaviour
+    {
+        [SerializeField] protected string _targetTag = "Player";
+        [SerializeField] protected UnityEvent<GameObject> _onEnter;
+        [SerializeField] protected UnityEvent<GameObject> _onStay;
+        [SerializeField] protected UnityEvent<GameObject> _onExit;
 
-		public virtual event UnityAction<GameObject> OnEnter {
-			add => _onEnter.AddListener(value);
-			remove => _onEnter.RemoveListener(value);
-		}
+        [SerializeField] private bool _shouldKillPlayer;
 
-		public virtual event UnityAction<GameObject> OnStay {
-			add => _onStay.AddListener(value);
-			remove => _onStay.RemoveListener(value);
-		}
+        public virtual event UnityAction<GameObject> OnEnter
+        {
+            add => _onEnter.AddListener(value);
+            remove => _onEnter.RemoveListener(value);
+        }
 
-		public virtual event UnityAction<GameObject> OnExit {
-			add => _onExit.AddListener(value);
-			remove => _onExit.RemoveListener(value);
-		}
+        public virtual event UnityAction<GameObject> OnStay
+        {
+            add => _onStay.AddListener(value);
+            remove => _onStay.RemoveListener(value);
+        }
 
-		protected virtual void OnCollisionEnter2D(Collision2D other)
-		{
-			if (other.gameObject.CompareTag(_targetTag))
-			{
-				_onEnter.Invoke(other.gameObject);
-			}
-		}
+        public virtual event UnityAction<GameObject> OnExit
+        {
+            add => _onExit.AddListener(value);
+            remove => _onExit.RemoveListener(value);
+        }
 
-		protected virtual void OnCollisionStay2D(Collision2D other)
-		{
-			if (other.gameObject.CompareTag(_targetTag))
-			{
-				_onStay.Invoke(other.gameObject);
-			}
-		}
+        protected virtual void OnCollisionEnter2D(Collision2D other)
+        {
+            if (other.gameObject.CompareTag(_targetTag))
+            {
+                _onEnter.Invoke(other.gameObject);
+                if (_shouldKillPlayer)
+                {
+                    var respawn = other.gameObject.GetComponent<Respawnable>();
+                    if (respawn != null)
+                    {
+                        respawn.Respawn();
+                    }
+                }
+            }
+        }
 
-		protected virtual void OnCollisionExit(Collision other)
-		{
-			if (other.gameObject.CompareTag(_targetTag))
-			{
-				_onExit.Invoke(other.gameObject);
-			}
-		}
-	}
+        protected virtual void OnCollisionStay2D(Collision2D other)
+        {
+            if (other.gameObject.CompareTag(_targetTag))
+            {
+                _onStay.Invoke(other.gameObject);
+                if (_shouldKillPlayer)
+                {
+                    var respawn = other.gameObject.GetComponent<Respawnable>();
+                    if (respawn != null)
+                    {
+                        respawn.Respawn();
+                    }
+                }
+            }
+        }
+
+        protected virtual void OnCollisionExit(Collision other)
+        {
+            if (other.gameObject.CompareTag(_targetTag))
+            {
+                _onExit.Invoke(other.gameObject);
+            }
+        }
+    }
 }
